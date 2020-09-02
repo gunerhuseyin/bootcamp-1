8592264384cc1403e0702ef32971b8ef01746b1d
huseyin guner <huseyin.guner@trendyol.com>
2020-09-02 13:58:14 +0300

    fix

--- src/main/java/org/kodluyoruz/trendyol/b/functions/good/Main.java
+++ src/main/java/org/kodluyoruz/trendyol/b/functions/good/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Sms sms = new Sms();
         AtomicInteger counter = new AtomicInteger(1);
-        sms.Send("Trendyol", "Kodluyoruz", "Bootcamp", "bootcamp@kodluyoruz.org", "01234567890", "Hello!");
+
+        SendSmsDTO sendSmsDTO = new SendSmsDTO();
+
+        sendSmsDTO.setName("Kodluyoruz");
+        sendSmsDTO.setSurname("Bootcamp");
+        sendSmsDTO.setSender("Trendyol");
+        sendSmsDTO.setEmail("bootcamp@kodluyoruz.org");
+        sendSmsDTO.setPhone("01234567890");
+        sendSmsDTO.setMessage("Hello!");
+
+        sms.Send(sendSmsDTO);
     }
 }
